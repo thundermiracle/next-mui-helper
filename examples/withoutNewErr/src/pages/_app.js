604b566ba782fa8cea09f457a727d42acdbb2bf0
thundermiracle <cissoliu@gmail.com>
2019-08-01 21:10:14 +0900

    #26 add example of prevent next9.0.2 from throwing err

---
+++ examples/withoutNewErr/src/pages/_app.js
@@ -0,0 +1,4 @@
+import NextApp from 'next/app';
+import { makeNextApp } from 'next-mui-helper';
+
+export default makeNextApp(undefined, undefined, undefined, undefined, NextApp);
---
+++ examples/withoutNewErr/src/pages/_document.js
@@ -0,0 +1,4 @@
+import NextDocument from 'next/document';
+import { makeNextDocument } from 'next-mui-helper';
+
+export default makeNextDocument(undefined, NextDocument);
